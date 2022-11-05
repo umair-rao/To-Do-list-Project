c05a2a2b0794dc4e384f275a22139d5d2b9d1fdf
umair-rao <umairmuqeem@gmail.com>
2022-11-05 13:56:33 +0500

    Linters error remove

--- src/index.js
+++ src/index.js
@@ -5,33 +5,32 @@ window.addEventListener('load', () => {
   const input = document.querySelector('#item-add');
   const listEl = document.querySelector('.taskContainer');
 
-  form.addEventListener('submit', (e) => {  e.preventDefault();
+  form.addEventListener('submit', (e) => {  
+    e.preventDefault();
 
-  const task = input.value;
+    const task = input.value;
 
-  const task_el = document.createElement('div');
-  task_el.classList.add('task');
+    const task_el = document.createElement('div');
+    task_el.classList.add('task');
 
-  const task_content_el = document.createElement('div');
-  task_content_el.classList.add('list-items');
+    const taskContent = document.createElement('div');
+    taskContent.classList.add('list-items');
 
-  task_el.appendChild(task_content_el);
+    task_el.appendChild(taskContent);
 
-  const task_check = document.createElement('input');
-  task_check.type = 'checkbox';
-  task_content_el.appendChild(task_check);
+    const task_check = document.createElement('input');
+    task_check.type = 'checkbox';
+    taskContent.appendChild(task_check);
 
-  const task_input_el = document.createElement('input');
-  task_input_el.classList.add('add-item');
-  task_content_el.type = 'text';
-  task_input_el.value = task;
-  task_input_el.setAttribute('readonly', 'readonly');
+    const task_input_el = document.createElement('input');
+    task_input_el.classList.add('add-item');
+    taskContent.type = 'text';
+    task_input_el.value = task;
+    task_input_el.setAttribute('readonly', 'readonly');
 
-  task_content_el.appendChild(task_input_el);
+    taskContent.appendChild(task_input_el);
 
-  listEl.appendChild(task_content_el);
-	});
+    listEl.appendChild(taskContent);
 });
-  
-
+});
   
