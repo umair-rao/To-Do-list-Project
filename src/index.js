639541bf58dca8655b4227c1486f0c9478209821
umair-rao <umairmuqeem@gmail.com>
2022-11-05 14:13:27 +0500

    Error remove

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ window.addEventListener('load', () => {
   const form = document.querySelector('.add-list');
   const input = document.querySelector('#item-add');
   const listEl = document.querySelector('.taskContainer');
-  form.addEventListener('submit', (e) => {  
+
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const task = input.value;
