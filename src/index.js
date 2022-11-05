77d0e134862c296728de324699026a557f6398d3
umair-rao <umairmuqeem@gmail.com>
2022-11-05 16:20:52 +0500

    Add task in array

--- src/index.js
+++ src/index.js
@@ -1,35 +1,72 @@
-import './style.css';
+import "./style.css";
 
-window.addEventListener('load', () => {
-  const form = document.querySelector('.add-list');
-  const input = document.querySelector('#item-add');
-  const listEl = document.querySelector('.taskContainer');
+window.addEventListener("load", () => {
+  const form = document.querySelector(".add-list");
+  const input = document.querySelector("#item-add");
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const task = input.value;
+    const taskEnter = input.value;
+    const tasks = [
+      {
+        description: "Take breakfast",
+        completed: true,
+        index: "1",
+      },
+      {
+        description: "Take Lunch",
+        completed: false,
+        index: "2",
+      },
+      {
+        description: "Take Dineer",
+        completed: true,
+        index: "3",
+      },
+    ];
 
-    const taskEl = document.createElement('div');
-    taskEl.classList.add('task');
+    const listEl = document.querySelector(".taskContainer");
 
-    const taskContent = document.createElement('div');
-    taskContent.classList.add('list-items');
+    tasks.forEach((element) => {
+      const taskContent = document.createElement("div");
+      taskContent.classList.add("task");
+      taskContent.innerHTML = ` 
+	  	<input type="checkbox">
+	  	${element.description}
+		  `;
+      listEl.appendChild(taskContent);
+    });
 
-    taskEl.appendChild(taskContent);
+    //     const task = input.value;
 
-    const taskCheck = document.createElement('input');
-    taskCheck.type = 'checkbox';
-    taskContent.appendChild(taskCheck);
+    //     const taskEl = document.createElement("div");
+    //     taskEl.classList.add("task");
 
-    const taskInput = document.createElement('input');
-    taskInput.classList.add('add-item');
-    taskContent.type = 'text';
-    taskInput.value = task;
-    taskInput.setAttribute('readonly', 'readonly');
+    //     const taskContent = document.createElement("div");
+    //     taskContent.classList.add("list-items");
 
-    taskContent.appendChild(taskInput);
+    //     taskEl.appendChild(taskContent);
 
-    listEl.appendChild(taskContent);
+    //     const taskCheck = document.createElement("input");
+    //     taskCheck.type = "checkbox";
+    //     taskContent.appendChild(taskCheck);
+
+    //     const taskInput = document.createElement("input");
+    //     taskInput.classList.add("add-item");
+    //     taskContent.type = "text";
+    //     taskInput.value = task;
+    //     taskInput.setAttribute("readonly", "readonly");
+
+    //     taskContent.appendChild(taskInput);
+
+    //     listEl.appendChild(taskContent);
   });
 });
+
+{
+  /* <div class="list-items" id="tasks">
+         <input type="checkbox">
+        <input id="new-item" class="add-item" placeholder="Add to your list..." type="text">
+</div> */
+}
