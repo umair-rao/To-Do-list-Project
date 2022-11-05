ea938978d4c7931943ce8ec9a1116267e6ebcff7
umair-rao <umairmuqeem@gmail.com>
2022-11-05 20:45:38 +0500

    Linters error remove

--- src/module/complete.js
+++ src/module/complete.js
@@ -1,11 +1,11 @@
 const complete = (index, value) => {
   const listOfTodo = JSON.parse(localStorage.getItem('todoList'));
   listOfTodo.forEach((todo) => {
-  if (todo.index === index) {
+    if (todo.index === index) {
       todo.completed = value;
     }
   });
-	localStorage.setItem('todoList', JSON.stringify(listOfTodo));
-  };
-  
+  localStorage.setItem('todoList', JSON.stringify(listOfTodo));
+};
+
 export default complete;
